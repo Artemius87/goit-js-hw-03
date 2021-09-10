002acaa1eda018fd0718f9f5758834e2e3c6a98d
Artem Haylo <avic.artem@gmail.com>
2021-09-10 17:14:34 +0300

    1 задача есть

--- Js/task-1.js
+++ Js/task-1.js
@@ -1,6 +1,33 @@
+// const user = {
+//   name: "Mango",
+//   age: 20,
+//   hobby: "html",
+//   premium: true,
+// };
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = "false";
+
+// console.log(user);
+// const entries = Object.entries(user);
+// for (const entry of entries) {
+//   const key = entry[0];
+//   const value = entry[1];
+//   console.log(`${key}: ${value}`);
+// }
+
 const user = {
   name: "Mango",
   age: 20,
   hobby: "html",
   premium: true,
 };
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = "false";
+
+for (const key in user) {
+  console.log(`${key}:${user[key]}`);
+}
